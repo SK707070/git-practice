801e24615b64d0252756b5b2f48f48e2f76c10f7
SK707070 <saikiran9744@gmail.com>
2025-05-20 11:26:53 +0530

    git created

---
+++ variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:  Hey $PERSON2 how are you doing"
+echo "$PERSON2:: Hello $PERSON1 I'm doing good how about you"
+echo "$PERSON1:  nothing just the routine just logged in getting the tasks for the day"
+echo "$PERSON2:: Oh great good to hear i was just logged in by the way"
+echo "$PERSON1:  ok then will see you back once all the tasks were done"
+echo "$PERSON2:: Good to hear that, See you soon"
+echo "$PERSON1:: Good to hear that, See you soon"
+echo "$PERSON1:: Good to hear that, See you soon"
+echo  nothing as of now
