9d2040a9187a0c973d7d00c7ff8262d3f4ecf778
SK707070 <saikiran9744@gmail.com>
2025-05-20 11:01:03 +0530

    git created

--- conversation.sh
+++ conversation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Narayana
-PERSON2=SaiKiran
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1:Hey $PERSON2 how are you doing"
 echo "$PERSON2:: Hello $PERSON1 I'm doing good how about you"
