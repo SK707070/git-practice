607e6bdccff60ef86fe2154844dfb081ab704287
SK707070 <saikiran9744@gmail.com>
2025-05-20 09:02:09 +0530

    git created

--- conversation.sh
+++ conversation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$PERSON1=SAI
-$PERSON2=KIRAN
+$PERSON1=sai
+$PERSON2=kiran
 
 echo "$PERSON1:Hey $PERSON2 how are you doing"
 echo "$PERSON2:: Hello $PERSON1 I'm doing good how about you"
@@ -9,4 +9,4 @@ echo "$PERSON1: nothing just the routine just logged in getting the tasks for th
 echo "$PERSON2:: Oh great good to hear i was just logged in by the way"
 echo "$PERSON1: ok then will see you back once all the tasks were done"
 echo "$PERSON2:: Good to hear that, See you soon"
-echo "$PERSON2:: Good to hear that, See you soon"
+echo "$PERSON1:: Good to hear that, See you soon"
