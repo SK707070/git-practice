01b526acb8eb9499a1ce69f631d10139c2c05c00
SK707070 <saikiran9744@gmail.com>
2025-05-21 06:59:38 +0530

    secial variables created

--- special-variables.sh
+++ special-variables.sh
@@ -2,3 +2,10 @@
 
 echo "All variables passed in this script: $@"
 echo "Number of variable: $#"
+echo "script name: $0"
+echo "current directory: $pwd"
+echo "user running this script: $user"
+echo "home directory user: $home"
+echo "pid of the scripts: $$"
+sleep 10 &
+echo "pid for last command: $!" 
